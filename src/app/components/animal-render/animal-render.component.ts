ce5dae9549ed6c2771bc26397253b9ef7351e1ad
Nicole <nicolejardim.c@gmail.com>
2025-06-10 14:01:52 -0300

    Add files via upload

---
+++ src/app/components/animal-render/animal-render.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-render',
+  templateUrl: './animal-render.component.html',
+  styleUrls: ['./animal-render.component.css']
+})
+export class AnimalRenderComponent {
+
+}
